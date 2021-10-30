f80be80d412dfcec8ca550e072b0a28ad9b01875
salbedaiwi <sumayaalbedaiwi@gmail.com>
2021-10-30 16:36:32 -0500

    list

--- popup.js
+++ popup.js
@@ -1,3 +1,14 @@
-var testButton = document.createElement("BUTTON");
-testButton.innerHTML = "THIS DOES NOTHING";
-document.body.appendChild(testButton);
+// var testButton = document.createElement("BUTTON");
+// testButton.innerHTML = "THIS DOES NOTHING";
+// document.body.appendChild(testButton);
+
+//<script>
+var counter= 4;
+
+function addelement() {
+    var completelist = document.getElementById("thelist");
+
+    completelist.innerHTML += "<li>Item " + counter + "</li>";
+    counter++;
+}
+//</script>
