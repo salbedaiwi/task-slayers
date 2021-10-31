be5df45e0b041dbf7bd0a67bf800feb41e258e9a
KaayaRane <63211165+KaayaRane@users.noreply.github.com>
2021-10-31 10:59:31 -0500

    d

--- popup.js
+++ popup.js
@@ -1,14 +1,25 @@
-var testButton = document.createElement("BUTTON");
-testButton.innerHTML = "THIS DOES NOTHING";
-document.body.appendChild(testButton);
+// var testButton = document.createElement("BUTTON");
+// testButton.innerHTML = "THIS DOES NOTHING";
+// document.body.appendChild(testButton);
 
-//<script>
-var counter= 4;
-
-function addelement() {
-    var completelist = document.getElementById("thelist");
-
-    completelist.innerHTML += "<li>Item " + counter + "</li>";
-    counter++;
+function addItem() {
+    // var ul = document.getElementById("dynamic-list");
+    // var candidate = document.getElementById("candidate");
+    // var li = document.createElement("li");
+    // li.setAttribute('id', candidate.value);
+    // li.appendChild(document.createTextNode(candidate.value));
+    // ul.appendChild(li);
+    // ul.innerHTML += "<li>Item " + counter + "</li>";
+    // counter++;
+    
 }
-//</script>
+var addBTN = document.getElementById("addItem");
+    addBTN.addEventListener("click", function(){
+        //alert("did something");
+        var ul = document.getElementById("dynamic-list");
+        var candidate = document.getElementById("candidate");
+        var li = document.createElement("li");
+        li.setAttribute('id', candidate.value);
+        li.appendChild(document.createTextNode(candidate.value));
+        ul.appendChild(li);
+    });
