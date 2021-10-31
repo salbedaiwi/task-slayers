21969c18b2ae5446121ab7d6290c1b2d31e5f998
salbedaiwi <sumayaalbedaiwi@gmail.com>
2021-10-31 09:52:22 -0500

    ADD BUTTON WORKS

--- popup.js
+++ popup.js
@@ -2,15 +2,24 @@
 // testButton.innerHTML = "THIS DOES NOTHING";
 // document.body.appendChild(testButton);
 
-//<script>
 function addItem() {
-    var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
-    var li = document.createElement("li");
-    li.setAttribute('id', candidate.value);
-    li.appendChild(document.createTextNode(candidate.value));
-    ul.appendChild(li);
+    // var ul = document.getElementById("dynamic-list");
+    // var candidate = document.getElementById("candidate");
+    // var li = document.createElement("li");
+    // li.setAttribute('id', candidate.value);
+    // li.appendChild(document.createTextNode(candidate.value));
+    // ul.appendChild(li);
     // ul.innerHTML += "<li>Item " + counter + "</li>";
     // counter++;
+    
 }
-//</script>
+var addBTN = document.getElementById("addItem");
+    addBTN.addEventListener("click", function(){
+        //alert("did something");
+        var ul = document.getElementById("dynamic-list");
+        var candidate = document.getElementById("candidate");
+        var li = document.createElement("li");
+        li.setAttribute('id', candidate.value);
+        li.appendChild(document.createTextNode(candidate.value));
+        ul.appendChild(li);
+    });
